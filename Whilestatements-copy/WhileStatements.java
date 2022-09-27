41c40f2ec064d3e20a9dc7b421493889cce3f34b
Colin Miller <colin.miller@mrjaffesclass.com>
2022-09-27 11:00:35 -0700

    doloops

--- Whilestatements-copy/WhileStatements.java
+++ Whilestatements-copy/WhileStatements.java
@@ -4,29 +4,27 @@ public class WhileStatements
     int b = 2;
     int c = 1;
     int d = -8;
-    
+
     public void run() {
         int i = 0;
-        while (i < 15) {
+        do {
             System.out.println("i is " + i + " which is < 15");
             i++;
-        }
+        } while (i < 15);
         int k = 10;
-        while (k > 5) {
+        do {
             System.out.println("k is " + k + " which is > 5");
             k--;
-        }
+        } while (k > 5);
         int a = 3;
         int b = 8;
-        while (a < 10) {
-            while (b <= 20) {
-                System.out.println("a is " + a + " which is < 10, " + "and b is " + b + " which is < or = 20");
-                a++;
-                b++;
-            }
-        }
+        do { 
+            System.out.println("a is " + a + " which is < 10, " + "and b is " + b + " which is < or = 20");
+            a++;
+            b++;
+        } while (a < 10 & b <= 20);
     }
-    
+
     public static void main(String[] args) 
     {
         WhileStatements hw = new WhileStatements();
